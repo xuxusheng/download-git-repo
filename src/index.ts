01e45387d1c284cc74ca591e7af38ba81f26dc37
许盛 <20691718@qq.com>
2021-12-02 20:21:40 +0800

    chore: tmp

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log('asdb')
+/**
+ * Add protocol to url
+ * @param origin
+ * @param clone
+ */
+function addProtocol(origin: string, clone: boolean): string {
+  if (!/^(f|ht)tps?:\/\//i.test(origin)) {
+    if (clone) {
+      origin = 'git@' + origin
+    } else {
+      origin = 'https://' + origin
+    }
+  }
+
+  return origin
+}
+
+function getUrl(repo, clone: boolean) {
+  let url
+
+  // Get origin with protocol and add trailing slash or colon (for ssh)
+  let origin = addProtocol(repo.origin, clone)
+
+  if (/^git@/i.test(origin)) {
+    origin = origin + ':'
+  } else {
+    origin = origin + '/'
+  }
+}
